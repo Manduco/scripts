09f0c3b38b6e4ac9d53477d147e236d0a8da6f9d
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-08-28 11:39:50 -0700

    restart windows oc
    gives user a pop on the forced restart

---
+++ restart.ps1
@@ -0,0 +1,11 @@
+﻿$wshell = New-Object -ComObject Wscript.Shell
+
+$wshell.Popup("This Server is scheduled for restart",10,"Save Data",0x0)
+
+$wshell.Popup("30 seconds to shutdown",2,"Save any data or it will be lost",0x0)
+
+$xCmdString = {sleep 30}
+
+Invoke-Command $xCmdString
+
+Restart-Computer -Force
