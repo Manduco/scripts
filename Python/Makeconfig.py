f2d51b1ba3b6188e76bcad918ef35dedda72c323
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-09-06 15:36:54 -0400

    more updates

--- Python/Makeconfig.py
+++ Python/Makeconfig.py
@@ -6,6 +6,7 @@ print(" --- Make configs ---")
 
 version = 0.0
 version= get_version()
+
 localIP = raw_input("Enter Local IP : ")
 GetHost = " "
 
@@ -31,15 +32,21 @@ def config1():
             "!\n")
 
 def secure():
-    f.write(
-        "hostname cisco28\n"
-        + "!\n"
+    f.write( "hostname cisco28\n" + "!\n"
         +"boot-start-marker\n"
-        +"boot-end-marker\n"
-        +"!\n"
+        +"boot-end-marker\n" +"!\n"
         +"logging buffered 128000 \n"
         +"enable secret 5 $1$wgM4$hnI4TqvqWv8EwjDWUgsjQ1\n"
         +"enable password something\n"+"!\n")
+
+def config2():
+    f.write( "aaa new-model \n"+"!\n"+"aaa authentication login default local\n"
+            +"aaa authentication login sdm_vpn_xauth_ml_1 local "
+            +"aaa authorization exec default local
+            +aaa authorization network sdm_vpn_group_ml_1 local !
+            +aaa session-id common !
+            +dot11 syslog no ip source-route !
+            +ip cef !")
 
 
 
@@ -50,6 +57,7 @@ if __name__ == "__main__":
     #write()
     config1()
     secure()
+    config2()
 
 
 
