0862880f146a7da8658ba2ab90af3af257beaccc
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-09-06 15:21:50 -0400

    updates

--- Python/Makeconfig.py
+++ Python/Makeconfig.py
@@ -1,16 +1,51 @@
+print(" --- Make configs ---")
 
-print(" ")
-print("working")
-print(" ")
+version = 0.0
+localIP = raw_input("Enter Local IP : ")
+GetHost = " "
 
-print ("complete")
-
-
-f = open("config.txt", "a")
-
-f.write("Now the\n")
-f.write("\n")
-f.write("test2 \n")
+config1= " "
 
 
-f.close()
+#opens or creates config txt file
+f = open("config.txt", "w")
+
+def write():
+    f.write("IP given" + localIP +"\n")#
+def close():
+
+    f.close()
+    print ("Configs Created")
+
+def config1():
+    f.write("version " + str(version) + "\n"
+            + "no service pad service \n"
+            + "timestamps debug datetime msec \n"
+            + "service timestamps log datetime msec \n"
+            + "no service password-encryption \n"+
+            "!\n")
+
+def secure():
+    f.write(
+        "hostname cisco28\n"
+        + "!\n"
+        +"boot-start-marker\n"
+        +"boot-end-marker\n"
+        +"!\n"
+        +"logging buffered 128000 \n"
+        +"enable secret 5 $1$wgM4$hnI4TqvqWv8EwjDWUgsjQ1\n"
+        +"enable password something\n"+"!\n")
+    
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+
+    #write()
+    config1()
+    secure()
+
+
+
+    close()
