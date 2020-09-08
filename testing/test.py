2d6d21c4032fddb2e306e3171914b217dd941796
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-09-08 16:33:40 -0400

    updates

--- testing/test.py
+++ testing/test.py
@@ -8,7 +8,12 @@ print("Version: "+ str(data["code"]["version"]))
 
 hostname = input("enter host name :")
 
-if( hostname == 'cisco28'):
-    print(data["vpnSubnet"]["cisco28"])
+# if( hostname == 'cisco28'):
+#     print(data["vpnSubnet"]["cisco28"])
+# else:
+#     print("this host name does not exist would like to create one?")
+
+if hostname in data["host"]:
+    print("hostname found")
 else:
-    print("this host name does not exist would like to create one?")
+    print("hostname was not found")
