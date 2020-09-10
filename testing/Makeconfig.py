869652247e36c973fe310e25e3aa25bff802d5d3
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-09-09 23:39:02 -0400

    upgrade

--- testing/Makeconfig.py
+++ testing/Makeconfig.py
@@ -2,10 +2,8 @@ import json
 
 #list of vars
 version = 0.0
-GetHost = " "
-config1= " "
-hostname = " "
-global data
+GetHost = "null"
+hostname = "null"
 
 print("---config script---")
 #opens or creates config txt file
@@ -14,15 +12,23 @@ f = open("config.txt", "w")
 def GetJsonData():
     with open('var.json') as f:
         data = json.load(f)
+
     version = data["code"]["version"]
     print("Version: " + str(version) )
-    #make()
+    global hostname
     hostname = input("enter host name :")
     if hostname in data["host"]:
-        print("hostname found")
-        print("Would like to create script with current settings?")
+        print("-hostname found")
+        answer = input("create configs with current settings? y/n : ")
+        if (answer == 'y'):
+            print("do something")
+        else:
+            print("making configs with current data")
     else:
         print("hostname was not found")
+
+def GetUserInput():
+    print("getuser input")
 
 def writef():
     f.write("IP given" + localIP +"\n")#
@@ -40,12 +46,13 @@ def config1():
             + "no service password-encryption \n"+
             "!\n")
 def secure():
-    f.write( "hostname cisco28\n" + "!\n"
+    f.write( "hostname "+hostname+"\n" + "!\n"
         +"boot-start-marker\n"
         +"boot-end-marker\n" +"!\n"
         +"logging buffered 128000 \n"
         +"enable secret 5 $1$wgM4$hnI4TqvqWv8EwjDWUgsjQ1\n"
         +"enable password something\n"+"!\n")
+        #remove both secret 5 key and password
 def config2():
     f.write( "aaa new-model \n"+"!\n"
             +"aaa authentication login default local\n"
@@ -54,23 +61,24 @@ def config2():
             +"aaa authorization network sdm_vpn_group_ml_1 local \n!\n"
             +"aaa session-id common \n!\n"
             +"dot11 syslog no ip source-route \n!\n"
-            +"ip cef \n!\n")
+            +"ip cef \n!\n"
+            +"no ip bootp server \n"
+            +"ip domain name firma.com \n"+ "\n"
+            +"ip host client-vpn 10.1.1.133"
+            +"ip name-server 10.1.1.33"
+            +"!\n"
+            +"multilink bundle-name authenticated\n"
+            +"!"
+            +"license udi pid CISCO2821 sn FCZ012345KM \n"
+            +"username user1 password 0 kamil1 \n"
+            +"username spravce privilege 15 password 0 kamil15 \n"
+            +"username user2 password 0 kamil2 \n"
+            +"!\n"
+            +"redundancy \n"
+            +"! \n")
+
 def crypto():
-    f.write( "no ip bootp server \n"
-    +"ip domain name firma.com \n"+ "\n"
-    +"ip host client-vpn 10.1.1.133"
-    +"ip name-server 10.1.1.33"
-    +"!\n"
-    +"multilink bundle-name authenticated\n"
-    +"!"
-    +"license udi pid CISCO2821 sn FCZ012345KM \n"
-    +"username user1 password 0 kamil1 \n"
-    +"username spravce privilege 15 password 0 kamil15 \n"
-    +"username user2 password 0 kamil2 \n"
-    +"!\n"
-    +"redundancy \n"
-    +"! \n"
-    +"crypto ikev2 diagnose error 50 \n"
+    f.write( "crypto ikev2 diagnose error 50 \n"
     +"!\n"
     +"crypto isakmp policy 1\n"
     +"encr 3des\n"
@@ -109,26 +117,112 @@ def crypto():
     +"crypto map SDM_CMAP_1 65535 ipsec-isakmp dynamic SDM_DYNMAP_1 \n"
     +"!\n")
 #end cisco cmd list
-
-#ask to keep current config or make new
-def make():
-    hostname = input("enter host name :")
-    if hostname in data["host"]:
-        print("hostname found")
-        print("Would like to create script with current settings?")
-    else:
-        print("hostname was not found")
+def config3():
+    f.write( "!"
+        +"redundancy\n"
+        +"!\n"
+        +"crypto ikev2 diagnose error 50\n"
+        +"!\n"
+        +"crypto isakmp policy 1\n"
+        +"encr 3des\n"
+        +"authentication pre-share\n"
+        +"group 2\n"
+        +"!\n"
+        +"crypto isakmp policy 3\n"
+        +"encr 3des\n"
+        +"group 2\n"
+        +"!\n"
+        +"crypto isakmp policy 10\n"
+        +"encr 3des\n"
+        +"hash md5\n"
+        +"authentication pre-share\n"
+        +"group 2\n"
+        +"!\n"
+        +"crypto isakmp policy 20\n"
+        +"encr 3des\n"
+        +"hash md5\n"
+        +"group 2\n"
+        +"!\n"
+        +"crypto isakmp client configuration group Group159\n"
+        +"key Key159Key\n"
+        +"pool SDM_POOL_1\n"
+        +"acl 100\n"
+        +"!\n"
+        +"crypto ipsec transform-set 3DES-MD5 esp-3des esp-md5-hmac\n"
+        +"!\n"
+        +"crypto dynamic-map SDM_DYNMAP_1 1\n"
+        +"set transform-set 3DES-MD5\n"
+        +"reverse-route\n"
+        +"!\n"
+        +"crypto map SDM_CMAP_1 client authentication list sdm_vpn_xauth_ml_1\n"
+        +"crypto map SDM_CMAP_1 isakmp authorization list sdm_vpn_group_ml_1\n"
+        +"crypto map SDM_CMAP_1 client configuration address respond\n"
+        +"crypto map SDM_CMAP_1 65535 ipsec-isakmp dynamic SDM_DYNMAP_1\n"
+        +"!\n"
+        +"interface Loopback10\n"
+        +"description For VPN Client\n"
+        +"ip address 192.168.201.1 255.255.255.0\n"
+        +"!\n"
+        +"interface GigabitEthernet0/0\n"
+        +"description $FW_OUTSIDE$\n"
+        +"ip address 10.1.1.220 255.255.255.0\n"
+        +"duplex auto\n"
+        +"speed auto\n"
+        +"crypto map SDM_CMAP_1\n"
+        +"!\n"
+        +"interface GigabitEthernet0/1\n"
+        +"description $ETH-LAN$$FW_INSIDE$\n"
+        +"ip address 192.168.220.1 255.255.255.0\n"
+        +"duplex auto\n"
+        +"speed auto\n"
+        +"!\n"
+        +"ip local pool SDM_POOL_1 192.168.200.1 192.168.200.10\n"
+        +"ip forward-protocol nd\n"
+        +"ip http server\n"
+        +"ip http secure-server\n"
+        +"!\n"
+        +"ip route 0.0.0.0 0.0.0.0 10.1.1.1\n"
+        +"!\n"
+        +"access-list 10 remark Prisup na router \n"
+        +"access-list 10 remark Pristup na router\n"
+        +"access-list 10 permit 10.1.1.0 0.0.0.255\n"
+        +"access-list 10 permit 192.168.201.0 0.0.0.255\n"
+        +"access-list 10 permit 192.168.200.0 0.0.0.255\n"
+        +"access-list 100 remark SDM_ACL Category=4\n"
+        +"access-list 100 permit ip 192.168.201.0 0.0.0.255 192.168.200.0 0.0.0.255\n"
+        +"no cdp run\n"
+        +"!\n"
+        +"control-plane\n"
+        +"!\n"
+        +"line con 0\n"
+        +"line aux 0\n"
+        +"line vty 0 4\n"
+        +"access-class 10 in\n"
+        +"exec-timeout 3600 0\n"
+        +"password kamil\n"
+        +"transport input all\n"
+        +"!\n"
+        +"scheduler allocate 20000 1000\n"
+        +"ntp server 10.1.1.1 \n"
+        +"!\n"
+        +"webvpn context Default_context\n"
+        +"ssl authenticate verify all\n"
+        +"!\n"
+        +"no inservice\n"
+        +"!\n"
+        +"end")
 
 
 if __name__ == "__main__":
     # execute only if run as a script
 
     GetJsonData()
-    #make()
     #write()
-    #config1()
-    #secure()
-    #config2()
-    #crypto()
+
+    config1()
+    secure()
+    config2()
+    crypto()
+    config3()
 
     #close()
