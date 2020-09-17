6251845ab0b8ae445d9b1b3a3cdbc4e60c07d7e3
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-09-17 17:53:33 -0400

    Add files via upload

--- Python/findfiles.py
+++ Python/findfiles.py
@@ -4,18 +4,18 @@ from pydub import AudioSegment
 
 count = 0
 #working dir
-Dir_To_Search = "testfile"
+Dir_To_Search = "testfile/129578200000"
 os.chdir(Dir_To_Search)
 
 print(" Converting..")
 for file in glob.glob("*.wav"):
     #print("found :",file)
-    dst = "convert"+str(count)+".mp3"
+    #dst = "convert"+str(count)+".mp3"
     name_of_file = file.split(".")
-    name_convert = name_of_file[0] +".mp3"
+    name_convert = name_of_file[0] + ".mp3"
 
     sound = AudioSegment.from_wav(file)
-    sound.export(name_convert, format="mp3")
+    sound.export(name_convert, format = "mp3")
 
-    print(file+ " >> "+name_convert)
+    print(file + " >> " + name_convert)
     count+= 1
